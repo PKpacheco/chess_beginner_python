a7f76c0829f0ec209ec1f95b4ab94456f62ed50c
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2016-12-05 11:28:29 -0300

    logic game

--- main.py
+++ main.py
@@ -23,3 +23,51 @@ class Model(object):
             f = self.board[start.i][start.j]
             self.board[destination.i][destination.j] = f
 
+
+class BoardLocation(object):
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+
+
+class View(object):
+    def display(self, board):
+        print("%s: %s" % (" ", column_reference))
+        print("-" * 43)
+        for i, row in enumerate(board):
+            row_marker = 8 - i
+            print("%s: %s" % (row_marker, row))
+
+
+class Controller(object):
+    def __init__(self):
+        self.model = Model()
+        self.view = View()
+
+    def run(self):
+        while True:
+            self.view.display(self.model.board)
+            move = raw_input("move (exemple e2-e5 or quit) ")
+            move = move.lower()
+            if move == "quit":
+                break
+            start, destination = self.basic_movement(move)
+            self.model.move(start, destination)
+
+    def basic_movement(self, move):
+        s, d = move.split("-")
+
+        i = 8 - int(s[-1])
+        j = column_reference.index(s[0])
+        start = BoardLocation(i, j)
+
+        i = 8 - int(d[-1])
+        j = column_reference.index(d[0])
+        destination = BoardLocation(i, j)
+
+        return start, destination
+
+
+if __name__ == "__main__":
+    C = Controller()
+    C.run()
